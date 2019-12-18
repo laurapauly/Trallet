20f3d45f0b8a75dda3a11d03eaf7594794464516
laurapauly <56833809+laurapauly@users.noreply.github.com>
2019-12-18 14:46:45 +0100

    Add theming to GlobalStyles and Storybook (#28)

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import GlobalStyles from '../src/GlobalStyles';
 import { withKnobs } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'emotion-theming';
+import light from '../src/themes/light';
 
 // add GlobalStyle for every story
 const GlobalStyleDecorator = storyFn => (
-  <>
+  <ThemeProvider theme={light}>
     <GlobalStyles />
     {storyFn()}
-  </>
+  </ThemeProvider>
 );
 addDecorator(GlobalStyleDecorator);
 addDecorator(withKnobs);
