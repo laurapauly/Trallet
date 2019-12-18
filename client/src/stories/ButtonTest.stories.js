394792761ca4a12cd7ea7e0ae63173ae5e61aaf6
laurapauly <56833809+laurapauly@users.noreply.github.com>
2019-12-18 16:29:49 +0100

    Design (#29)
    * Add Google Font/Roboto
    
    * Add icons and testicon to Storybook

---
+++ client/src/stories/ButtonTest.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlusButton from '../components/Button.js';
+
+export default {
+  title: 'Plusbutton'
+};
+
+export function Button() {
+  return <PlusButton></PlusButton>;
+}
