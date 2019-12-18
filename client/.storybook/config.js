d5611740630bf19cecead626a55767f33148f0a1
laurapauly <laurapauly@arcor.de>
2019-12-18 12:09:54 +0100

    Add Globalstyles to Storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,6 +1,18 @@
-import { configure } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import GlobalStyles from '../src/GlobalStyles';
+import { withKnobs } from '@storybook/addon-knobs';
+
+// add GlobalStyle for every story
+const GlobalStyleDecorator = storyFn => (
+  <>
+    <GlobalStyles />
+    {storyFn()}
+  </>
+);
+addDecorator(GlobalStyleDecorator);
+addDecorator(withKnobs);
+// automatically import all files ending in *.stories.js
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
 
 // automatically import all files ending in *.stories.js
-addDecorator(withKnobs);
-configure(require.context("../src/stories", true, /\.stories\.js$/), module);
