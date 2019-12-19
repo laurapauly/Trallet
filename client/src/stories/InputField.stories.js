f8e4a954915a28738e9ca7cad96a797648444668
laurapauly <laurapauly@arcor.de>
2019-12-19 11:21:10 +0100

    Add Inputfield

---
+++ client/src/stories/InputField.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import InputField from '../components/InputField.js';
+
+export default {
+  title: 'Inputfieldtext'
+};
+
+export function Input() {
+  return <InputField></InputField>;
+}
