9ec7e9537f499456726d354867bce3e9e82cd2dc
laurapauly <laurapauly@arcor.de>
2019-12-23 14:27:47 +0100

    Modified Inputfield to Select Box

---
+++ client/src/components/SelectField.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import light from '../themes/light.js';
+
+const Select = styled.select`
+  width: 281px;
+  height: 35px;
+  background-color: ${light.colors.backgroundprimary};
+  border-radius: 10px;
+  color: ${light.colors.iconinactive};
+  outline: none;
+  border: none;
+  font-size: 10px;
+  text-align: left;
+  padding: 5px;
+  margin: 5px;
+  margin-bottom: 10px;
+`;
+
+export default function SelectField() {
+  return (
+    <Select class="ui dropdown">
+      <option>Essen</option>
+      <option>Transport</option>
+      <option>Flüge</option>
+      <option>Hotel</option>
+      <option>Party</option>
+      <option>Einkauf</option>
+      <option>Freizeit</option>
+      <option>Sonstiges</option>
+    </Select>
+  );
+}
