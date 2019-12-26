d86019b241e8cddd7905aa765a410899a75cfd26
laurapauly <laurapauly@arcor.de>
2019-12-26 11:32:37 +0100

    Add main pages

---
+++ client/src/pages/Journeys.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Journeys() {
+  return (
+    <>
+      <div>See journeys here</div>
+    </>
+  );
+}
---
+++ client/src/pages/SpendingList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SpendingList() {
+  return (
+    <>
+      <div>See spendings here</div>
+    </>
+  );
+}
