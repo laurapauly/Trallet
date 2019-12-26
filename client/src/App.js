629d2587993269ad4752f7bbe674fb3d18b76a72
laurapauly <laurapauly@arcor.de>
2019-12-26 11:33:05 +0100

    Add basic page structure with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,40 @@ import NavBarFooter from './components/NavBarFooter.js';
 import GlobalStyles from './GlobalStyles';
 import light from './themes/light.js';
 import { ThemeProvider } from 'emotion-theming';
-import SelectField from './components/SelectField.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Journeys from './pages/Journeys.js';
+import SpendingList from './pages/SpendingList.js';
+import Statistics from './pages/Statistics.js';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column wrap;
+`;
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={light}>
         <GlobalStyles />
-        <SelectField />
-
-        <NavBarFooter></NavBarFooter>
+        <Router>
+          <Container>
+            <Switch>
+              <Route exact path="/">
+                <SpendingList />
+              </Route>
+              <Route exact path="/journeys">
+                <Journeys />
+              </Route>
+              <Route exact path="/statistics">
+                <Statistics />
+              </Route>
+            </Switch>
+            <NavBarFooter></NavBarFooter>
+          </Container>
+        </Router>
       </ThemeProvider>
     </>
   );
