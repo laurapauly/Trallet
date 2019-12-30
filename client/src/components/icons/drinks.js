86fca019ea7c8e480094a6ba63e893655a72519c
laurapauly <laurapauly@arcor.de>
2019-12-30 15:09:04 +0100

    Add missing props

--- client/src/components/icons/drinks.js
+++ client/src/components/icons/drinks.js
@@ -11,7 +11,7 @@ const Svg = styled.svg`
 
 export default function DrinkIcon(props) {
   return (
-    <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
       <path d="M21 5V3H3v2l8 9v5H6v2h12v-2h-5v-5l8-9zM7.43 7L5.66 5h12.69l-1.78 2H7.43z" />
       <path fill="none" d="M0 0h24v24H0z" />
     </Svg>
