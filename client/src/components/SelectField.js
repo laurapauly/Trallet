2a04d307c73e7ffd66753e6d8d132ac0f8708680
laurapauly <laurapauly@arcor.de>
2019-12-30 15:26:58 +0100

    Change width from px to %

--- client/src/components/SelectField.js
+++ client/src/components/SelectField.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import light from '../themes/light.js';
 
 const Select = styled.select`
-  width: 281px;
+  width: 100%;
   height: 35px;
   background-color: ${light.colors.backgroundprimary};
   border-radius: 10px;
