816df85e437f908608c8b88e73fcd9e414cd815e
laurapauly <laurapauly@arcor.de>
2020-01-05 13:18:44 +0100

    fix error

--- client/src/lib/GroupSpendings.js
+++ client/src/lib/GroupSpendings.js
@@ -3,10 +3,11 @@ export default function GroupSpendings(spendings) {
   spendings.map(spending => {
     if (typeof result[spending.date] === 'undefined') {
       result[spending.date] = [spending];
-      return;
+      return null;
     }
 
     result[spending.date].push(spending);
+    return null;
   });
   return result;
 }
