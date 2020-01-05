7928b01d54d868b8dd631ab7b713ce4a1eb54a5c
laurapauly <laurapauly@arcor.de>
2020-01-05 11:29:23 +0100

    Add dummy data to test

---
+++ client/src/components/Spendings.js
@@ -0,0 +1,45 @@
+export const spendings = [
+  {
+    '2019-10-01': [
+      {
+        "title": "Test",
+        "amount": "234566",
+        "date": "2020-01-08",
+        "location": "test",
+        "category": "food",
+        "itemId": "1",
+        "id": 47
+      },
+      {
+        "title": "Test",
+        "amount": "234566",
+        "date": "2020-01-08",
+        "location": "test",
+        "category": "food",
+        "itemId": "1",
+        "id": 47
+      },
+    ],
+  },
+  '2019-10-02': [
+    {
+      "title": "Test",
+      "amount": "234566",
+      "date": "2020-01-08",
+      "location": "test",
+      "category": "food",
+      "itemId": "1",
+      "id": 47
+    },
+    {
+      "title": "Test",
+      "amount": "234566",
+      "date": "2020-01-08",
+      "location": "test",
+      "category": "food",
+      "itemId": "1",
+      "id": 47
+    },
+  ],
+},
+]
