1ceeac2af02eccfcbdb42f3f767dcd62cc1c2832
laurapauly <laurapauly@arcor.de>
2020-01-06 10:26:04 +0100

    Outsource FormContainer

---
+++ client/src/components/FormContainer.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const FormContainer = styled.div`
+  padding: 20px 5px 20px 5px;
+  margin-bottom: 200px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  backdrop-filter: blur(5px);
+`;
+
+export default FormContainer;
