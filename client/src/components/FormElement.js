a20a5f410b6fd05da878c06bfb91cbe1068fc2a2
laurapauly <laurapauly@arcor.de>
2020-01-06 10:26:21 +0100

    Outsource FormElement

---
+++ client/src/components/FormElement.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import light from '../themes/light.js';
+
+const FormElement = styled.form`
+  width: 100%;
+  box-shadow: 0px 3px 20px #00000029;
+  background-color: ${light.colors.basic};
+  height: 550px;
+  padding: 20px 30px 20px 30px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export default FormElement;
