06e70c6add02879506ff577670db0584f158caaf
laurapauly <laurapauly@arcor.de>
2020-01-07 15:08:02 +0100

    Edit card styling

--- client/src/components/JourneyCard.js
+++ client/src/components/JourneyCard.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import SeeMoreIcon from './icons/seemore.js';
 import light from '../themes/light.js';
-import PhotoIcon from './icons/addphoto.js';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-const CardElement = styled.div`
-  height: 100px;
+const CardElement = styled(Link)`
+  all: unset;
+  background-color: ${props => props.theme.colors.basic};
   border-radius: 10px;
   box-shadow: 0pt 3pt 20pt #00000029;
   display: flex;
@@ -15,65 +14,64 @@ const CardElement = styled.div`
   align-items: center;
   padding: 5px;
   background-color: ${light.colors.basic};
-  margin: 40px 20px 40px 20px;
+  margin: 10px 20px 10px 20px;
   position: relative;
   z-index: 10;
 `;
 
-const Picture = styled.div`
+const Picture = styled.img`
   height: 65px;
   width: 100px;
-  background-color: ${light.colors.fontsmall};
+  background-color: ${light.colors.fontSmall};
   border-radius: 5px;
   text-align: center;
 `;
 
-const Wrapper = styled.div`
+const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
 `;
 
-const Destination = styled.h6`
-  margin-top: 35px;
+const Destination = styled.p`
+  all: unset;
+  font-size: 16px;
+  margin-top: 10px;
   display: block;
-  font-size: 0.83em;
-  margin-block-start: 1.67em;
-  margin-block-end: 0;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
+
   font-weight: bold;
 `;
 
-const Wrapper2 = styled.div`
+const DetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 20px;
+  margin-top: 10px;
   margin-bottom: 20px;
 `;
 
 const Details = styled.p`
-  display: block;
-  margin-block-start: 0em;
-  margin-block-end: 0em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  font-size: 8pt;
+  all: unset;
+
+  font-size: 10pt;
+  margin-top: 5px;
+  display: flex;
 `;
 
-const IconButton = styled(Link)`
-  width: 25px;
-  height: 25px;
-  outline: none;
-  border: none;
-  background-color: ${light.colors.basic};
-  margin-bottom: 50px;
+const Highlight = styled.p`
+  all: unset;
+  font-weight: bold;
+  color: ${light.colors.action};
+  margin-right: 3px;
+`;
+
+const CardContainer = styled.div`
+  margin-top: 20px;
 `;
 
 export default function JourneyCard() {
   const [journeyItems, setJourneyItems] = useState([]);
   async function getJourneyItems() {
-    const response = await fetch('http://localhost:4000/items');
+    const response = await fetch('http://localhost:4040/journeys');
     const newJourneys = await response.json();
     setJourneyItems(newJourneys);
   }
@@ -81,26 +79,34 @@ export default function JourneyCard() {
   React.useEffect(() => {
     getJourneyItems();
   }, []);
+
+  const link = function(id) {
+    return `/journeys/${id}`;
+  };
+
   return (
     <div>
       {journeyItems.map(item => (
-        <CardElement key={item.id}>
-          <Picture>
-            <PhotoIcon />
-          </Picture>
-          <Wrapper>
-            <Destination>{item.title}</Destination>
-            <Wrapper2>
-              <Details>
-                {item.date1} - {item.date2}
-              </Details>
-              <Details>{item.budget} verfügbar</Details>
-            </Wrapper2>
-          </Wrapper>
-          <IconButton to="/">
-            <SeeMoreIcon />
-          </IconButton>
-        </CardElement>
+        <CardContainer key={item.id}>
+          <CardElement to={link(item.id)}>
+            <Picture
+              src="https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+              alt="Journey Picture"
+            ></Picture>
+            <ContentContainer>
+              <Destination>{item.title}</Destination>
+              <DetailsContainer>
+                <Details>
+                  {item.startDate} - {item.endDate}
+                </Details>
+                <Details>
+                  <Highlight>{item.budget}</Highlight>
+                  verfügbar
+                </Details>
+              </DetailsContainer>
+            </ContentContainer>
+          </CardElement>
+        </CardContainer>
       ))}
     </div>
   );
