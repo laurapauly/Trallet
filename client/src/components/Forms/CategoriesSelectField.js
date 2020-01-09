71b0ad03ab12cea8b7a388ce7924e174621f5dc0
laurapauly <laurapauly@arcor.de>
2020-01-09 14:12:24 +0100

    Change name of file, map options and add onChange function

---
+++ client/src/components/Forms/CategoriesSelectField.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import categories from '../categories';
+import PropTypes from 'prop-types';
+
+const Select = styled.select`
+  width: 100%;
+  height: 35px;
+  background-color: ${props => props.theme.colors.backgroundPrimary};
+  border-radius: 10px;
+  color: ${props => props.theme.colors.iconInactive};
+  outline: none;
+  border: none;
+  font-size: 10px;
+  text-align: left;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+export default function CategoriesSelectField({ onChange }) {
+  return (
+    <Select onChange={onChange}>
+      {categories.map(category => (
+        <option key={category.value} value={category.value}>
+          {category.title}
+        </option>
+      ))}
+      ;
+    </Select>
+  );
+}
+
+CategoriesSelectField.propTypes = {
+  onChange: PropTypes.func
+};
