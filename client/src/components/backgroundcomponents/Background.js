be20140c151bfad9e667b2593c06c01e4b6068ee
laurapauly <laurapauly@arcor.de>
2020-01-09 17:06:21 +0100

    Outsource background elements

---
+++ client/src/components/backgroundcomponents/Background.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import WaveIcon from '../icons/WaveIcon';
+import PropTypes from 'prop-types';
+
+const JourneysBackground = styled.header`
+  padding-bottom: 6rem;
+  width: 100%;
+  position: relative;
+`;
+
+const StyleWaveIcon = styled(WaveIcon)`
+  position: absolute;
+  bottom: 0;
+`;
+
+export default function Background({ children }) {
+  return (
+    <JourneysBackground>
+      <StyleWaveIcon />
+      {children}
+    </JourneysBackground>
+  );
+}
+
+Background.propTypes = {
+  children: PropTypes.element
+};
