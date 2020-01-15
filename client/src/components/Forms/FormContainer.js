8f9b90a933619007964301bfba24c6ee39833295
laurapauly <laurapauly@arcor.de>
2020-01-15 09:59:41 +0100

    Add zoom in animation for all forms

--- client/src/components/Forms/FormContainer.js
+++ client/src/components/Forms/FormContainer.js
@@ -4,7 +4,7 @@ const FormContainer = styled.div`
   padding: 20px 5px 20px 5px;
   margin-bottom: 200px;
   position: fixed;
-  top: 0;
+  top: 0vh;
   left: 0;
   z-index: 999;
   display: flex;
@@ -12,6 +12,7 @@ const FormContainer = styled.div`
   width: 100vw;
   height: 100vh;
   backdrop-filter: blur(5px);
+  animation: zoomIn 0.5s;
 `;
 
 export default FormContainer;
