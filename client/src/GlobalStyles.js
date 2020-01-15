132f8d9aafd5680d16d00fa6af95d0eb32d25b35
laurapauly <laurapauly@arcor.de>
2020-01-15 09:56:49 +0100

    Add keyframes to global styles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -20,8 +20,26 @@ function GlobalStyles() {
           background: ${theme.colors.backgroundprimary};
           font-family: 'Roboto', sans-serif;
         }
+        @keyframes zoomIn {
+          from {
+            opacity: 0;
+            transform: scale3d(0.3, 0.3, 0.3);
+          }
+
+          50% {
+            opacity: 1;
+          }
+        }
       `}
     />
   );
 }
 export default GlobalStyles;
+
+// @keyframes slide {
+//   0% {
+//     top: 70vh;
+//   }
+//   100% {
+//     top: 9vh;
+//   }
